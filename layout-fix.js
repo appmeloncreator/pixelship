b45c2566e85b312b8635a5baf688b9cbf9a8034e
appmeloncreator <melonoquestions@gmail.com>
2026-07-06 14:03:35 -0400

    Limit create sidebar to eight images

--- layout-fix.js
+++ layout-fix.js
@@ -24,7 +24,7 @@
   renderMine=function(){
     const mine=state.user?state.posts.filter(post=>post.userId===state.user.id):[];
     $('#myGrid').innerHTML=mine.length
-      ?mine.map(post=>'<div class="mini" data-id="'+post.id+'"><img src="'+post.image+'" alt="'+esc(post.prompt)+'"><span class="mini-status '+(post.isPublic?'public':'')+'">'+(post.isPublic?'PUBLIC':'PRIVATE')+'</span><div class="mini-tools"><button data-action="edit">EDIT</button><button data-action="'+(post.isPublic?'private':'publish')+'">'+(post.isPublic?'MAKE PRIVATE':'PUBLISH')+'</button><button class="danger" data-action="delete">DELETE</button></div></div>').join('')+'<div class="mini empty">＋ NEW IDEA</div>'
+      ?mine.slice(0,8).map(post=>'<div class="mini" data-id="'+post.id+'"><img src="'+post.image+'" alt="'+esc(post.prompt)+'"><span class="mini-status '+(post.isPublic?'public':'')+'">'+(post.isPublic?'PUBLIC':'PRIVATE')+'</span><div class="mini-tools"><button data-action="edit">EDIT</button><button data-action="'+(post.isPublic?'private':'publish')+'">'+(post.isPublic?'MAKE PRIVATE':'PUBLISH')+'</button><button class="danger" data-action="delete">DELETE</button></div></div>').join('')+'<div class="mini empty">＋ NEW IDEA</div>'
       :'<div class="mini empty">'+(state.user?'YOUR NEXT IDEA':'SIGN IN TO CREATE')+'</div>';
     renderLibrary();
   };
